6ec76ef3de83449e48269636892b2b9b86dee44a
Dilşad <91341704+dilsad-erdogan@users.noreply.github.com>
2024-12-18 21:13:01 +0300

    hero mobile design

--- client/src/components/hero/Hero.jsx
+++ client/src/components/hero/Hero.jsx
@@ -46,15 +46,15 @@ const Hero = () => {
                             <div key={data.id}>
                                 <div className="grid grid-cols-1 sm:grid-cols-2">
                                     {/* Content section */}
-                                    <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
-                                        <h1 className="text-5xl sm:text-6xl lg:text-7xl font-bold text-white text-end" style={{ fontFamily: "'Lucida Handwriting', cursive" }}>{data.title}</h1>
-                                        <p className="text-xl sm:text-5xl lg:text-xl font-bold text-white break-words max-w-md">{data.desc}</p>
+                                    <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 p-5 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10">
+                                        <h1 className="text-3xl sm:text-6xl lg:text-7xl font-bold text-white text-end" style={{ fontFamily: "'Lucida Handwriting', cursive" }}>{data.title}</h1>
+                                        <p className="text-sm sm:text-lg lg:text-xl font-bold text-white break-words max-w-xs sm:max-w-md lg:max-w-lg">{data.desc}</p>
                                         <button className="inline-block px-6 py-2 max-w-[200px] font-semibold text-white bg-yellow-500 hover:bg-yellow-600 rounded-xl duration-200">Sipariş Ver</button>
                                     </div>
 
                                     {/* Image section */}
                                     <div className="order-1 sm:order-2">
-                                        <img src={data.img} alt="" className="w-[500px] h-[500px] sm:h-[600px] sm:w-[600px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40" />
+                                        <img src={data.img} alt="" className="w-[300px] h-[300px] sm:h-[500px] sm:w-[500px] sm:scale-105 lg:scale-110 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40" />
                                     </div>
                                 </div>
                             </div>
